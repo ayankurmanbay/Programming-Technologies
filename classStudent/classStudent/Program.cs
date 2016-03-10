dfe5b1680509eab21a2ef1cb9a37a4b00395ff53
Аян Курманбай < >
2016-03-11 00:49:44 +0600

    added class Student

---
+++ classStudent/classStudent/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classStudent
+{
+    class Program
+    {
+        class Student
+        {    
+            public string first_name;
+            public string last_name;
+            public string id;
+            public int year;
+            public double gpa;
+            public Student(string first_name, string last_name, string id, int year, double gpa)
+            {
+                this.first_name = first_name;
+                this.last_name = last_name;
+                this.id = id;
+                this.year = year;
+                this.gpa = gpa;
+            }
+            public override string ToString()
+            {
+                return "First Name: " + this.first_name + " Last Name: " + this.last_name + " ID: " 
+                    +  this.id + " Year of study: " + this.year + " GPA: " + this.gpa;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Student firstStudent = new Student("Ayan", "Kurmanbay", "15BD02093", 1, 3.99);
+            Console.WriteLine(firstStudent);
+            Console.ReadKey();
+        }
+    }
+}
