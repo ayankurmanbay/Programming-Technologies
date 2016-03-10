a0a16c1b3f193f65ea3fe3a59fb2d18af7606e3a
Аян Курманбай < >
2016-03-11 02:02:01 +0600

    added class student

--- classStudent/classStudent/Program.cs
+++ classStudent/classStudent/Program.cs
@@ -13,18 +13,20 @@ namespace classStudent
         public string id;
         public int year;
         public double gpa;
-        public Student(string first_name, string last_name, string id, int year, double gpa)
+        public string specialization;
+        public Student(string first_name, string last_name, string id, int year, double gpa, string specialization)
         {
             this.first_name = first_name;
             this.last_name = last_name;
             this.id = id;
             this.year = year;
             this.gpa = gpa;
+            this.specialization = specialization;
         }
         public override string ToString()
         {
             return "First Name: " + this.first_name + " Last Name: " + this.last_name + " ID: "
-                + this.id + " Year of study: " + this.year + " GPA: " + this.gpa;
+                + this.id + " Year of study: " + this.year + " GPA: " + this.gpa +  " Specialization: "+ specialization;
         }
 
     }
@@ -32,7 +34,7 @@ namespace classStudent
     {
         static void Main(string[] args)
         {
-            Student firstStudent = new Student("Ayan", "Kurmanbay", "15BD02093", 1, 3.99);
+            Student firstStudent = new Student("Ayan", "Kurmanbay", "15BD02093", 1, 3.99, "Information Systems");
             Console.WriteLine(firstStudent);
             Console.ReadKey();
         }
